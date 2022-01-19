6c53cbaa6ad85ed38cfeaf1bf5e2e1d1ef0022a1
Muhammet Emin <eminegah@gmail.com>
2022-01-19 13:39:18 +0300

    Update

---
+++ 11-MVC/MVC/MVC with Data/MVC with Data/Models/Classes/Unvan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCwithData.Models.Classes
+{
+    public class Unvan
+    {
+        public string UnvanId { get; set; }
+        public string UnvanAd { get; set; }
+
+    }
+}
