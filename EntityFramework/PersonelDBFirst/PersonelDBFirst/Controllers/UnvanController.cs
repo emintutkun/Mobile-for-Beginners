9d35848dd7151deeeb4a619511f5aa24e92e37cc
Muhammet Emin <eminegah@gmail.com>
2022-02-22 11:20:04 +0300

    Update

--- EntityFramework/PersonelDBFirst/PersonelDBFirst/Controllers/UnvanController.cs
+++ EntityFramework/PersonelDBFirst/PersonelDBFirst/Controllers/UnvanController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonelDBFirst.Connection;
 using PersonelDBFirst.Models.Data;
 using PersonelDBFirst.Models.Views;
 
@@ -11,7 +12,7 @@ namespace PersonelDBFirst.Controllers
     public class UnvanController : Controller
     {
         // GET: Unvan
-        perdb2Entities db = new perdb2Entities();
+        perdb2Entities db = DbSingleTone.GetConnection();
         public ActionResult List()
         {
             //1.yol
--- EntityFramework/PersonelDBFirst/PersonelDBFirst/Models/DTOs/PersonelDTO.cs
+++ EntityFramework/PersonelDBFirst/PersonelDBFirst/Models/DTOs/PersonelDTO.cs
@@ -10,9 +10,11 @@ namespace PersonelDBFirst.Models.DTOs
         public int Id { get; set; }
         public string Ad { get; set; }
         public string Soyad { get; set; }
+        public decimal Maas { get; set; }
         public int UnvanId { get; set; }
         public string UnvanAd { get; set; }
         public string Ikamet { get; set; }
         public string Yonetici { get; set; }
+        public string UlkeAd { get; set; }
     }
 }
